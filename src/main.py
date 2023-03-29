8ef38586233dced7aa4fdda4d3427ef98d1012df
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-03-28 23:45:47 -0300

    update: Initial data analysis models' experiments

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-# from datamining.data_visualization import
-# from datamining.preprocessing import
-# from datamining.ml_methods import
+from datamining.data_visualization import plot_distribution
+from datamining.data_visualization import protocol_dist
+from datamining.preprocessing import discretize_values
+from datamining.preprocessing import url_protocol
+from datamining.ml_methods import holdout_split
+from datamining.ml_methods import decision_tree
+from datamining.ml_methods import svm_model
 
 from warnings import filterwarnings
+from pandas import read_csv
 from os.path import isdir
 from os import getcwd
 from os import chdir
 from os import mkdir
-import pandas as pd
 
 
 filterwarnings('ignore')
@@ -18,8 +22,29 @@ if not isdir(f'{getcwd()}/models'):
 
 def main():
     
-    print('Data Mining Start!')
-    # url_dataset = pd.read_csv(f'{getcwd()}/datasets/URLs.csv')
+    url_dataset = read_csv(f'{getcwd()}/datasets/test.csv')
+    class_names = ['benign', 'defacement', 'phishing', 'malware']
+
+    # plot_distribution(url_dataset)
+
+    attributes = url_dataset.drop(['type'], axis='columns')
+    attributes = discretize_values(attributes.copy(), 'url')
+
+    classes = url_dataset.drop(['url'], axis='columns') 
+    classes = discretize_values(classes.copy(), 'type')
+
+    # url_dataset['protocol'] = url_dataset['url'].apply(lambda x: url_protocol(x))
+    # attributes = url_dataset.drop(['type'], axis='columns')
+    # attributes = discretize_values(attributes.copy(), 'url')
+    # attributes = discretize_values(attributes.copy(), 'protocol')
+
+    # protocol_dist(url_dataset)
+
+    training_attributes, test_attributes, training_classes, test_classes = holdout_split(attributes, classes)
+    decision_tree(training_attributes, test_attributes, training_classes, test_classes, class_names)
+
+    training_attributes, test_attributes, training_classes, test_classes = holdout_split(attributes, classes)
+    svm_model(training_attributes, test_attributes, training_classes, test_classes, class_names)
 
 
 if __name__ == '__main__':
