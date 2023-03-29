9abdc293074ac6d231ee6cb4dece28376a06a132
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-03-28 23:38:55 -0300

    update: Added some text file processing commands

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,3 +3,11 @@
 sudo apt install python3 python3-pip build-essential python3-dev
 pip install -U scikit-learn
 pip install -r requirements.txt
+
+## Text processing ##
+
+# head -5000 URLs.csv > test.csv
+# fgrep -c benign URLs.csv
+# fgrep -c defacement URLs.csv
+# fgrep -c phishing URLs.csv
+# fgrep -c malware URLs.csv
