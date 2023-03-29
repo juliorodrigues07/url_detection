df2e162768dd60b3e4d0340c981157ca3df70b9c
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-03-28 23:41:12 -0300

    feat: Added plots about instances' classes and attributes ratio

---
+++ src/datamining/data_visualization.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_distribution(dataset):
+
+    # Statistical summary (mean, minimum, maximum, standard deviation, quartiles, ...)
+    # print(url_data.describe().transpose())
+
+    split = dataset.type.value_counts()
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    sns.barplot(x=split.index, y=split, palette='Reds')
+
+    ax.set_title('Class Distribution')
+    ax.set_ylabel("Amount", fontsize=18)
+    ax.set_xlabel("URL Types", fontsize=18)
+
+    ax.grid(which='both')
+    ax.grid(which='minor', alpha=0.8)
+    ax.grid(which='major', alpha=0.8)
+    plt.show()
+
+    # fig.savefig()
+
+
+def protocol_dist(dataset):
+
+    sns.countplot(x='protocol', data=dataset, palette='Greens')
+    plt.show()
