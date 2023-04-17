193d6e5599aefe484a73cc8afa86432f73dfaae1
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-04-17 10:54:58 -0300

    feat: Added new features to improve classification model

--- src/datamining/preprocessing.py
+++ src/datamining/preprocessing.py
@@ -18,29 +18,19 @@ def url_protocol(url):
     
     protocol = urlparse(url).scheme
 
-    # Identifying which protocol each site uses to construct a new feature
+    # Identifying which communication protocol each site uses
     match = str(protocol)
     if match == 'https':
-        return 'HTTPS'
+        return 2
     elif match == 'http':
-        return 'HTTP'
-    else:
-        return 'Not Specified'
-    
-    
-def find_anomalies(url):
-
-    domain = str(urlparse(url).hostname)
-    pattern = re.search(domain, url)
-
-    if pattern:
-        return 1
-    else:
         return 0
+    else:
+        return 1
 
 
 def url_length(dataset):
 
+    # Gets the brute length of each url
     dataset['url_length'] = dataset['url'].apply(lambda x: len(x))
     return dataset
 
@@ -49,8 +39,40 @@ def count_digits(url):
 
     total = 0
 
+    # Counts the number of digits present in each url
     for c in url:
         if c.isnumeric():
             total += 1
 
     return total
+
+
+def count_dirs(url):
+
+    # Counts the number of directories present in each url path
+    url_path = urlparse(url).path
+    return url_path.count('/')
+
+
+def first_directory(url):
+
+    url_path = urlparse(url).path
+
+    # Gets the brute length of each url first directory
+    try:
+        fd_length = len(url_path.split('/')[1])
+    except IndexError:
+        fd_length = 0
+
+    return fd_length
+
+
+def odd_words(url):
+
+    # Search for suspicious words related to phishing in each url
+    pattern = re.search('free|account|signin|bonus|lucky|extra|payment|details', url)
+
+    if pattern:
+        return 1
+    else:
+        return 0
