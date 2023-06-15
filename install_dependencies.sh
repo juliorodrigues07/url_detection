da42b5eeb7fcadd3ef3e4dd9fa4da28450b8c5c5
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-06-15 09:02:37 -0300

    update: Added text processing commands to concat datasets and remove duplicates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,10 +4,13 @@ sudo apt install python3 python3-pip build-essential python3-dev
 pip install -U scikit-learn
 pip install -r requirements.txt
 
-## Text processing ##
+## TEXT PROCESSING ##
 
 # head -5000 URLs.csv > test.csv
-# fgrep -c benign URLs.csv
-# fgrep -c defacement URLs.csv
 # fgrep -c phishing URLs.csv
-# fgrep -c malware URLs.csv
+
+# Concat the datasets in a temporary file (excludes header from second file)
+# awk '(NR == 1) || (FNR > 1)' kaggle/kaggle.csv phishtank/phishtank.csv > tmp.csv
+
+# Remove duplicate rows without sorting in a final file and removes the temporary one
+# awk '{if (!($0 in x)) {print $0; x[$0]=1} }' tmp.csv > merged.csv && rm tmp.csv
