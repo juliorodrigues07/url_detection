35dd465dc8410372e56fcb0635f5882560c3d136
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-08-03 16:10:43 -0300

    Chore: Removed unused dependencies

--- frontend/url-detector/src/App.vue
+++ frontend/url-detector/src/App.vue
@@ -1,28 +1,16 @@
 <template>
+
   <div id="app">
     <router-view/>
   </div>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- frontend/url-detector/src/main.js
+++ frontend/url-detector/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
 import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
