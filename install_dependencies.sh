14d9136f3448e78a95a634531915dcd91ac8f409
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-08-03 16:32:08 -0300

    Feat: Added new dependencies for the GUI and install commands

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,11 @@ sudo apt install python3 python3-pip build-essential python3-dev
 pip install -U scikit-learn
 pip install -r requirements.txt
 
+sudo apt-get install npm
+cd frontend/url-detector
+npm i
+cd .. && cd ..
+
 ## TEXT PROCESSING ##
 
 # head -5000 URLs.csv > test.csv
